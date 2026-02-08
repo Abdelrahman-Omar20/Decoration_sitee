e59c581ffc575035ed472dd029b505a68add1141
Abdelrahman-Omar20 <abderlhman.omar208@gmail.com>
2026-02-08 13:47:45 +0200

    chore: configure deployment for GitHub Pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+    base: '/Decoration_sitee/',
     plugins: [react()],
     resolve: {
         alias: {
